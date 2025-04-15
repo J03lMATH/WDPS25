cf836b4bc9e4d8aa363ce857e85cd5b70354fde8
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2025-04-15 09:43:34 -0400

    some form of problem with the login

--- public/main.js
+++ public/main.js
@@ -1,162 +1,178 @@
-class FeedBack {
-    constructor(username, feedback) {
-        this.username = username;
-        this.feedback = feedback;
+class User{
+    constructor(userId, username, email, fName, lName, password) {
+        this.userId = userId;
+        this.userName = username;
+        this.email = email;
+        this.fName = fName;
+        this.lName = lName;
+        this.password = password;
     }
 
+    getUserId() {
+        return this.userId;
+    }
+    setUserId(userId) {
+        this.userId = userId;
+    }
     getUsername() {
         return this.username;
     }
     setUsername(username) {
-        this.username = username;
+        this.userName = username;
     }
-
-    getFeedback() {
-        return this.feedback;
+    getEmail() {
+        return this.email;
     }
-    setFeedback(feedback) {
-        this.feedback = feedback;
+    setEmail(email) {
+        this.email = email;
     }
-
-    printFeedback() {
-        console.log(this.username + " : " + this.feedback);
+    getFName() {
+        return this.fName;
+    }
+    setFName(fName) {
+        this.fName = fName;
+    }
+    getLName() {
+        return this.lName;
+    }
+    setLName(lName) {
+        this.lName = lName;
+    }
+    getPassword() {
+       return this.password;
+    }
+    setPassword(password) {
+        this.password = password;
+    }
+    getFullName() {
+        return this.fName + " " + this.lName;
     }
 }
 
-let feedbackForm = document.getElementById("feedbackForm");
-feedbackForm.addEventListener("submit", submitFeedback);
+const users = [
+];
 
-function submitFeedback(e) {
+const user1 = new User(123, "JM15", "joel04mathew@gmail.com", "Joel", "Mathew", "12345678");
+const user2 = new User(132, "Joey", "joeDane@gmail,com", "Joe", "Dane", "12345831");
+
+console.log(user1.userId);
+console.log(user1); 
+console.log(user1.getFullName());
+
+console.log(users);
+
+users.push(user1);
+users.push(user2);
+
+console.log(users);
+
+
+users.forEach(function(users) {
+    console.log(users.getFullName());
+})
+
+let loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", login)
+
+function login(e) {
     e.preventDefault();
+   
+    //to prevent the form from submitting and refreshing the page
+    let errorSection= document.getElementById("errorMessage");
 
-    const username = document.getElementById("username").value;
-    const feedback = document.getElementById("feedback").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-    const fb = new FeedBack(username, feedback);
-    fb.printFeedback();
+    if(!validString(username)){
+
+        //to show error message if username is empty
+        errorSection.innerText = "Username is required";
+        return;
+    }
+    
+       
+        //login the user if username and password are not empty
+
+        errorSection.innerText =""
+        console.log(username);
+
+        const user = {
+            username: username,
+            password: password
+        }
+
+        console.log("User Info: ", user)
+
+
+    // Clear the input fields after submission
+    document.getElementById("username").value ="";
+    document.getElementById("password").value ="";
+
+}
+
+
+// Function to check if a string is empty
+function validString(word) {
+    return word.length > 0;
 }
 
 
 
-
-// class User{
-//     constructor(userId, username, email, fName, lName, password) {
-//         this.userId = userId;
-//         this.userName = username;
-//         this.email = email;
-//         this.fName = fName;
-//         this.lName = lName;
-//         this.password = password;
+// class FeedBack{
+//     constructor(username, feedback){
+//         this.username = username;
+//         this.feedback = feedback;
 //     }
 
-//     getUserId() {
-//         return this.userId;
-//     }
-//     setUserId(userId) {
-//         this.userId = userId;
-//     }
-//     getUsername() {
+//     getUsername(){
 //         return this.username;
 //     }
-//     setUsername(username) {
-//         this.userName = username;
+//     setUsername(username){
+//         this.username = username;
 //     }
-//     getEmail() {
-//         return this.email;
+
+//     getFeedback(){
+//         return this.feedback;
 //     }
-//     setEmail(email) {
-//         this.email = email;
+//     setFeedback(feedback){
+//         this.feedback = feedback;
 //     }
-//     getFName() {
-//         return this.fName;
-//     }
-//     setFName(fName) {
-//         this.fName = fName;
-//     }
-//     getLName() {
-//         return this.lName;
-//     }
-//     setLName(lName) {
-//         this.lName = lName;
-//     }
-//     getPassword() {
-//         return this.password;
-//     }
-//     setPassword(password) {
-//         this.password = password;
-//     }
-//     getFullName() {
-//         return this.fName + " " + this.lName;
+
+//     printFeedback(){
+//         console.log(this.username + " : " + this.feedback);
 //     }
 // }
 
-// const users = [
+// const feedbacks = [
+//     {
+//         username: "Joey",
+//         feedback: "Great service!"
+//     },
+//     {
+//         username: "JM15",
+//         feedback: "Very helpful!"
+//     }
 // ];
 
-// const user1 = new User(123, "JM15", "joel04mathew@gmail.com", "Joel", "Mathew", "12345678");
-// const user2 = new User(132, "Joey", "joeDane@gmail,com", "Joe", "Dane", "12345831");
+// console.log(feedbacks);
 
-// console.log(user1.userId);
-// console.log(user1); 
-// console.log(user1.getFullName());
-
-// console.log(users);
-
-// users.push(user1);
-// users.push(user2);
-
-// console.log(users);
-
-
-// users.forEach(function(users) {
-//     console.log(users.getFullName());
+// feedbacks.forEach((feedback) => {
+//     console.log(feedback.username + " : " + feedback.feedback);
 // })
 
-// // let loginForm = document.getElementById("loginForm");
-// // loginForm.addEventListener("submit", login)
+// let feedbackForm = document.getElementById("feedbackForm");
+// feedbackForm.addEventListener("submit", submitFeedback)
 
-// // function login(e) {
-// //     e.preventDefault();
-    
-// //     //to prevent the form from submitting and refreshing the page
-// //     let errorSection= document.getElementById("error!!");
+// function submitFeedback(e) {
+//     e.preventDefault();
 
-// //     let username = document.getElementById("username").value;
-// //     let password = document.getElementById("password").value;
+//     const username = document.getElementById("username").value;
+//     const feedback = document.getElementById("feedback").value;
 
-// //     if(validString(username)){
-
-// //         //to show error message if username is empty
-// //         errorSection.innerHTML = "Username is required";
-// //     }
-// //     else{
-        
-// //         //login the user if username and password are not empty
-
-// //         errorSection.innerText =""
-// //         console.log(username);
-
-// //         const user = {
-// //             username: username,
-// //             password: password
-// //         }
-
-// //         let section = document.getElementById("section");
-// //         section.innerHTML = 'Welcome to the site, ${username}!';
-
-// //         console.log("User Info: ", user)
-// //     }
-
-// //     // Clear the input fields after submission
-// //     document.getElementById("username").value ="";
-// //     document.getElementById("password").value ="";
-
-// // }
+//     feedbacks.push(new FeedBack(username, feedback));
+//     console.log(feedbacks);
+// }
 
 
-// // // Function to check if a string is empty
-// // function validString(word) {
-// //     return word == ""
-// // }
-
+ 
 
