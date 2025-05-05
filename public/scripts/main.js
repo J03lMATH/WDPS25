8c2643f6e971addc8e28fcd879bf8a2f5b408ec1
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2025-05-04 23:43:30 -0400

    rewriting the main js code and ading local storage and login capabilites

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,85 +1,17 @@
-class User{
-    constructor(userId, username, email, fName, lName, password) {
-        this.userId = userId;
-        this.userName = username;
-        this.email = email;
-        this.fName = fName;
-        this.lName = lName;
-        this.password = password;
-    }
-
-    getUserId() {
-        return this.userId;
-    }
-    setUserId(userId) {
-        this.userId = userId;
-    }
-    getUsername() {
-        return this.username;
-    }
-    setUsername(username) {
-        this.userName = username;
-    }
-    getEmail() {
-        return this.email;
-    }
-    setEmail(email) {
-        this.email = email;
-    }
-    getFName() {
-        return this.fName;
-    }
-    setFName(fName) {
-        this.fName = fName;
-    }
-    getLName() {
-        return this.lName;
-    }
-    setLName(lName) {
-        this.lName = lName;
-    }
-    getPassword() {
-       return this.password;
-    }
-    setPassword(password) {
-        this.password = password;
-    }
-    getFullName() {
-        return this.fName + " " + this.lName;
-    }
+async function fetchData(route ='', data ={}, methodType) {
+    const response = await fetch(`http://localhost:3000${route}`, {
+        method: methodType, // *POST, PUT, DELETE, etc.
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
+      if (response.ok) {
+        return await response.json(); // parses JSON response into native JavaScript objects
+      } else {
+        throw await response.json();
+      }
 }
-
-const users = [
-    {
-        userId: 123,
-        username: "JM15",
-        email: "joel04mathew@gmail.com",
-        fName: "Joel",
-        lName: "Mathew",
-        password: "coolPass",
-    },
-    {
-        userId: 132,
-        username: "Joey",
-        email: "joeDane@gmail.com",
-        fName: "Joe",
-        lName: "Dane",
-        password: "12345831",
-    }
-];
-
-//const user1 = new User(123, "JM15", "joel04mathew@gmail.com", "Joel", "Mathew", "12345678");
-//const user2 = new User(132, "Joey", "joeDane@gmail,com", "Joe", "Dane", "12345831");
-
-//praticing User values
-//console.log(user1.userId);
-//console.log(user1); 
-//console.log(user1.getFullName());
-
-//console.log(users);
-
-//users.push(user1);
-//users.push(user2);
 
 console.log(users);
 
@@ -87,6 +19,59 @@ console.log(users);
 users.forEach(function(users) {
     console.log(users.username + " : " + users.email + " : " + users.fName + " " + users.lName);
 })
+
+
+//loginForm
+let loginForm = document.getElementById("loginForm");
+if(loginForm){
+loginForm.addEventListener("submit", login)
+}
+
+function login(e) {
+    e.preventDefault();
+   
+    //to prevent the form from submitting and refreshing the page
+    let errorSection= document.getElementById("errorMessage");
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(!validString(username)){
+
+        //to show error message if username is empty
+        errorSection.innerText = "Username is required";
+    }
+    
+       
+        //login the user if username and password are not empty
+
+        const user = {
+            username: username,
+            password: password
+        }
+
+        fetchData('/users/login', user, 'POST')
+        .then(data => {
+            if(!data.message) {
+              setCurrentUser(data)
+              window.location.href = "food.html"
+            }
+          })
+          .catch(err => {
+            errorSection.innerText = `${err.message}`
+          })
+
+
+
+        console.log("User Info: ", user)
+
+
+    // Clear the input fields after submission
+    document.getElementById("username").value ="";
+    document.getElementById("password").value ="";
+
+}
+
 
 
 //Register Form
@@ -119,110 +104,26 @@ function register(e) {
     registerForm.reset();
 }
 
-//loginForm
-let loginForm = document.getElementById("loginForm");
-if(loginForm){
-loginForm.addEventListener("submit", login)
-}
-
-function login(e) {
-    e.preventDefault();
-   
-    //to prevent the form from submitting and refreshing the page
-    let errorSection= document.getElementById("errorMessage");
-
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-
-    if(!validString(username)){
-
-        //to show error message if username is empty
-        errorSection.innerText = "Username is required";
-    }
-    
-       
-        //login the user if username and password are not empty
-
-        console.log(username);
-
-        const user = {
-            username: username,
-            password: password
-        }
-
-        console.log("User Info: ", user)
-
-
-    // Clear the input fields after submission
-    document.getElementById("username").value ="";
-    document.getElementById("password").value ="";
-
-}
-
-
 // Function to check if a string is empty
 function validString(word) {
     return word.length > 0;
 }
 
 
+//local Storage Functions
 
- class FeedBack{
-     constructor(username, feedback){
-         this.username = username;
-         this.feedback = feedback;
-     }
+function setCurrentUser(user) {
+    localStorage.setItem('user', JSON.stringify(user))
+  }
+  
+  function getCurrentUser() {
+    return JSON.parse(localStorage.getItem('user'))
+  }
+  
+  function removeCurrentUser() {
+    localStorage.removeItem('user')
+  }
 
-     getUsername(){
-         return this.username;
-     }
-     setUsername(username){
-         this.username = username;
-     }
-
-     getFeedback(){
-         return this.feedback;
-     }
-     setFeedback(feedback){
-         this.feedback = feedback;
-     }
-
-     printFeedback(){
-         console.log(this.username + " : " + this.feedback);
-     }
- }
-
- const feedbacks = [
-     {
-         username: "Joey",
-         feedback: "Great service!"
-     },
-     {
-         username: "JM15",
-         feedback: "Very helpful!"
-     }
- ];
-
- console.log(feedbacks);
-
- feedbacks.forEach((feedback) => {
-     console.log(feedback.username + " : " + feedback.feedback);
- })
-
- let feedbackForm = document.getElementById("feedbackForm");
- if(feedbackForm){
- feedbackForm.addEventListener("submit", submitFeedback)
-    }
-
- function submitFeedback(e) {
-     e.preventDefault();
-
-     const username = document.getElementById("username").value;
-     const feedback = document.getElementById("feedback").value;
-
-     feedbacks.push(new FeedBack(username, feedback));
-     console.log(feedbacks);
- }
 
 
  
